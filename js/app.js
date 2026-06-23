845dee734ec6f1e8defd15f757fcb58190fa3136
Juan Carlos Montoya Guzman <juan.montoya@lim.upeu.edu.pe>
2026-06-23 10:06:57 -0500

    Detectar login J3K

--- js/app.js
+++ js/app.js
@@ -2,3 +2,4 @@ const SUPABASE_URL = "https://ssbsfqpujqjowrkbyntv.supabase.co";
 const SUPABASE_KEY = "sb_publishable_HSEG5YV7SPPmtePuHkx_Pw_56b9Ukw-";
 
 console.log("Supabase listo");
+console.log("Login J3K detectado");
