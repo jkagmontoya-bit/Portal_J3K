e7a01ef3e1cc74d80fa9609336bdc52fc044a98a
Juan Carlos Montoya Guzman <juan.montoya@lim.upeu.edu.pe>
2026-06-23 11:18:51 -0500

    Agregar Publishable Key

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
 const SUPABASE_URL = "https://ssbsfqpujqjowrkbyntv.supabase.co";
 
-console.log("J3K listo para conectar Supabase");
+const SUPABASE_KEY = "sb_publishable_HSEG5YV7SPPmtePuHkx_Pw_56b9Ukw-";
+
+console.log("Preparando conexión J3K");
