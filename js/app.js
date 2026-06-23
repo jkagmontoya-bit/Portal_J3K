3b4dc44b19d7453f0b1af9555125c17420b4bad4
Juan Carlos Montoya Guzman <juan.montoya@lim.upeu.edu.pe>
2026-06-23 10:29:28 -0500

    Prueba Supabase

--- js/app.js
+++ js/app.js
@@ -1,5 +1,13 @@
 const SUPABASE_URL = "https://ssbsfqpujqjowrkbyntv.supabase.co";
 const SUPABASE_KEY = "sb_publishable_HSEG5YV7SPPmtePuHkx_Pw_56b9Ukw-";
 
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+
 console.log("Supabase listo");
 console.log("Login J3K detectado");
+
+async function probarSupabase() {
+  console.log("Conexión Supabase OK");
+}
+
+probarSupabase();
