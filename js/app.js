0b280e39024165918849e2d22e92dc589c7c43ae
Juan Carlos Montoya Guzman <juan.montoya@lim.upeu.edu.pe>
2026-06-23 11:35:08 -0500

    Corregir inicializacion Supabase

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ const SUPABASE_URL = "https://ssbsfqpujqjowrkbyntv.supabase.co";
 
 const SUPABASE_KEY = "sb_publishable_HSEG5YV7SPPmtePuHkx_Pw_56b9Ukw-";
 
-const supabase = window.supabase.createClient(
+const supabaseClient = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
 
-console.log("Cliente Supabase listo");
+console.log("Cliente Supabase inicializado");
