057cd8eafeb83c61b56f18c8eecbb9ff25e19c00
Juan Carlos Montoya Guzman <juan.montoya@lim.upeu.edu.pe>
2026-06-23 09:57:17 -0500

    Configurar Supabase

--- js/app.js
+++ js/app.js
@@ -1,1 +1,4 @@
-console.log("Portal_J3K conectado");
+const SUPABASE_URL = "https://ssbsfqpujqjowrkbyntv.supabase.co";
+const SUPABASE_KEY = "sb_publishable_HSEG5YV7SPPmtePuHkx_Pw_56b9Ukw-";
+
+console.log("Supabase listo");
